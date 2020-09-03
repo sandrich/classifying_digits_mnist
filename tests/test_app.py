e394e34529dcb7acf35fab3332a62f9d646662a7
Christian Sandrini <mail@chrissandrini.ch>
2020-09-03 09:44:56 +0200

    Fix initial app tests and import issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,11 @@ from src import app
 class TestApp(unittest.TestCase):
     
     def test_train(self):
-        self.assertRaises(app.train([], [], 0, 1, 'entropy'), ValueError)
-        self.assertRaises(app.train([], [], 1, 0, 'entropy'), ValueError)
-        self.assertRaises(app.train([], [], 1, 1, 'test'), ValueError)
+        with self.assertRaises(ValueError):
+            _ = app.train([[1]], [1], 0, 1, 'entropy')
+        
+        with self.assertRaises(ValueError):
+            _ = app.train([[1]], [1], 1, 0, 'entropy')
+
+        with self.assertRaises(ValueError):
+            _ = app.train([[1]], [1], 1, 1, 'test')
